ed515cda7b554fc69a2ad30d71f9209b52611038
Liam Hays <liamrhays@gmail.com>
2024-01-29 12:31:32 -0700

    Add function tests to protobuf_codec.

--- src/protobuf_codec.rs
+++ src/protobuf_codec.rs
@@ -307,29 +307,162 @@ impl ProtobufCodec {
     }
 }
 
-#[cfg(test)]
-mod protobuf_codec_tests {
-    use crate::flipper_pb;
 
-    use super::ProtobufCodec;
-
-    #[test]
-    fn protobuf_codec_data_test() {
-        // check that data can be loaded in and out, from protobuf form to byte data
-        let mut p = ProtobufCodec::new();
-        let path = "/ext/app.fap";
-        let launch_packet = p.create_launch_request_packet(path).unwrap();
-        match ProtobufCodec::parse_response(&launch_packet) {
-            Ok(m) => {
-                if let Some(flipper_pb::flipper::main::Content::AppStartRequest(r)) = m.1.content {
-                    assert_eq!(path, r.name);
-                }
-            },
-            Err(e) => {
-                panic!("error {:?}", e);
-            }
-        };
-    }
-
+// Function (unit?) tests!
     
+#[test]
+fn protobuf_codec_launch_request_test() {
+    // check that data can be loaded in and out, from protobuf form to byte data
+    let mut p = ProtobufCodec::new();
+    // include command id increment in all tests
+    p.inc_command_id();
+    let path = "/ext/app.fap";
+    
+    let launch_packet = p.create_launch_request_packet(path).unwrap();
+    match ProtobufCodec::parse_response(&launch_packet) {
+        Ok(m) => {
+            if let Some(flipper_pb::flipper::main::Content::AppStartRequest(r)) = m.1.content {
+                assert_eq!(1, m.1.command_id);
+                assert_eq!(path, r.name);
+            }
+        },
+        Err(e) => {
+            panic!("error {:?}", e);
+        }
+    };
+}
+
+#[test]
+fn protobuf_codec_list_packet_test() {
+    let mut p = ProtobufCodec::new();
+    let path = "/ext/apps";
+    p.inc_command_id();
+    let launch_packet = p.create_list_request_packet(path).unwrap();
+    
+    match ProtobufCodec::parse_response(&launch_packet) {
+        Ok(m) => {
+            if let Some(flipper_pb::flipper::main::Content::StorageListRequest(r)) = m.1.content {
+                assert_eq!(1, m.1.command_id);
+                assert_eq!(path, r.path);
+            }
+        },
+        Err(e) => {
+            panic!("error {:?}", e);
+        }
+    };
+    
+}
+
+#[test]
+fn protobuf_codec_write_request_packet_test() {
+    //let mut p = ProtobufCodec::new();
+    //p.inc_command_id();
+    //let path = "/ext/apps/GPIO/ublox.fap";
+
+    //let write_request_packets =
+    todo!("do this bro");
+}
+
+#[test]
+fn protobuf_codec_read_request_test() {
+    let mut p = ProtobufCodec::new();
+    p.inc_command_id();
+    let path = "/ext/apps/GPIO/ublox.fap";
+    let launch_packet = p.create_list_request_packet(path).unwrap();
+    
+    match ProtobufCodec::parse_response(&launch_packet) {
+        Ok(m) => {
+            if let Some(flipper_pb::flipper::main::Content::StorageReadRequest(r)) = m.1.content {
+                assert_eq!(1, m.1.command_id);
+                assert_eq!(path, r.path);
+            }
+        },
+        Err(e) => {
+            panic!("error {:?}", e);
+        }
+    };
+}
+
+#[test]
+pub fn protobuf_codec_stat_request_test() {
+    let mut p = ProtobufCodec::new();
+    p.inc_command_id();
+    let path = "/ext/apps/GPIO/ublox.fap";
+    let launch_packet = p.create_list_request_packet(path).unwrap();
+
+    match ProtobufCodec::parse_response(&launch_packet) {
+        Ok(m) => {
+            if let Some(flipper_pb::flipper::main::Content::StorageReadRequest(r)) = m.1.content {
+                assert_eq!(1, m.1.command_id);
+                assert_eq!(path, r.path);
+            }
+        },
+        Err(e) => {
+            panic!("error {:?}", e);
+        }
+    };
+}
+
+#[test]
+pub fn protobuf_codec_alert_request_test() {
+    let mut p = ProtobufCodec::new();
+    p.inc_command_id();
+    let alert_packet = p.create_alert_request_packet().unwrap();
+    
+    match ProtobufCodec::parse_response(&alert_packet) {
+        Ok(m) => {
+            if let Some(flipper_pb::flipper::main::Content::SystemPlayAudiovisualAlertRequest(_)) = m.1.content {
+                assert_eq!(1, m.1.command_id);
+            } else {
+                panic!("unexpected type decoded!");
+            }
+        },
+        Err(e) => {
+            panic!("error {:?}", e);
+        }
+    };
+}
+
+
+#[test]
+pub fn protobuf_codec_set_datetime_request_test() {
+    let mut p = ProtobufCodec::new();
+    p.inc_command_id();
+    let datetime = chrono::DateTime::parse_from_rfc2822("Mon, 29 Jan 2024 10:39:45 -0700").unwrap();
+    let datetime_packet = p.create_set_datetime_request_packet(datetime).unwrap();
+
+    match ProtobufCodec::parse_response(&datetime_packet) {
+        Ok(m) => {
+            if let Some(flipper_pb::flipper::main::Content::SystemSetDatetimeRequest(r)) = m.1.content {
+                assert_eq!(r.datetime.hour, datetime.hour());
+                assert_eq!(r.datetime.minute, datetime.minute());
+                assert_eq!(r.datetime.second, datetime.second());
+                assert_eq!(r.datetime.day, datetime.day());
+                assert_eq!(r.datetime.month, datetime.month());
+                assert_eq!(r.datetime.year, datetime.year() as u32);
+                assert_eq!(r.datetime.weekday, datetime.weekday().number_from_monday());
+                assert_eq!(1, m.1.command_id);
+            }
+        },
+        Err(e) => {
+            panic!("error {:?}", e);
+        }
+    };
+}
+
+#[test]
+fn bad_data_test() {
+    // force the whole thing to u8
+    // the original data is from a StorageListRequest of "/ext/apps/NFC/"
+    //let dat = [18u8, 58, 16, 10, 14, 47, 101, 120, 116, 47, 97, 112, 112, 115, 47, 78, 70, 67, 47];
+    let dat = [18u8, 0, 16, 10, 14, 47, 101, 120, 116, 47, 97, 112, 112, 115, 47, 78, 70, 67, 47];
+
+    match ProtobufCodec::parse_response(&dat) {
+        Ok(_) => {
+            panic!("parse of bad data succeeded!");
+        },
+        Err(_) => {
+            // nothing to do here
+        }
+    };
 }
